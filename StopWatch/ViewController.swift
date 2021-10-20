48e8f4616280b779112709e73332d6f0eb0d8b1f
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-10-20 22:48:35 +0300

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Abdirizak Hassan on 10/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
